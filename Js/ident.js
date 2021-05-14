13fa327f5c51ac11e226a9041e0f26d042ca07d5
DepartamentoInves <84188676+DepartamentoInves@users.noreply.github.com>
2021-05-14 09:22:11 -0500

    Update ident.js

--- Js/ident.js
+++ Js/ident.js
@@ -1,9 +1,19 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyALlmuQk-drJKkiKU16ZMbAyJ8LCA1kaTo",
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+      apiKey: "AIzaSyALlmuQk-drJKkiKU16ZMbAyJ8LCA1kaTo",
     authDomain: "formulario-de-investigacion.firebaseapp.com",
     projectId: "formulario-de-investigacion",
     storageBucket: "formulario-de-investigacion.appspot.com",
     messagingSenderId: "847861615199",
     appId: "1:847861615199:web:90808530581e000548e9ea",
     measurementId: "G-2LBXNDB8WN"
-  };
+});
+
