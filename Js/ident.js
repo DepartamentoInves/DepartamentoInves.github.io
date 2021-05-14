9b2d8294ea53a52e5ea86444563ecaaf728ef641
DepartamentoInves <84188676+DepartamentoInves@users.noreply.github.com>
2021-05-14 09:04:42 -0500

    Create ident.js

---
+++ Js/ident.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyALlmuQk-drJKkiKU16ZMbAyJ8LCA1kaTo",
+    authDomain: "formulario-de-investigacion.firebaseapp.com",
+    projectId: "formulario-de-investigacion",
+    storageBucket: "formulario-de-investigacion.appspot.com",
+    messagingSenderId: "847861615199",
+    appId: "1:847861615199:web:90808530581e000548e9ea",
+    measurementId: "G-2LBXNDB8WN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
